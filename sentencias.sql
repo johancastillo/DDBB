eebd6f308ddc1091cc8b5bf0085d5cfe063ff467
Johan Castillo <jcjohyan2707@gmail.com>
2020-08-26 14:26:50 -0400

    Normalizacion Parte 1

---
+++ sentencias.sql
@@ -0,0 +1,30 @@
+/*
+¿Què tipo de entidades? autores
+Nombre de la tabla: autores
+*/
+
+/*
+Datos a almacenar de la entidad
+
+Nombre
+Genero
+Fecha de nacimiento
+Paìs de origen
+*/
+
+/*
+Columnas y tipo de dato
+
+nombre VARCHAR(50)
+genero CHAR(1)
+fecha_nacimiento DATE
+pais_origen VARCHAR(40)
+
+*/
+
+/*
+Normalización
+
+nombre VARCHAR(50)
+apellido VARCHAR()
+*/
