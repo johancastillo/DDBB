c1dc93d83fa441603fb61eb0bd4701bd78303ff4
Johan Castillo <jcjohan2707@gmail.com>
2020-09-02 10:51:39 -0400

    Valores unicos con UNIQUE

--- data.sql
+++ data.sql
@@ -6,13 +6,14 @@ CREATE TABLE IF NOT EXISTS autores(
   autor_id INT NOT NULL,
   nombre VARCHAR(25) NOT NULL,
   apellido VARCHAR(25) NOT NULL,
+  seudonimo VARCHAR(50) UNIQUE,
   genero CHAR(1) NOT NULL,
   fecha_nacimiento DATE NOT NULL,
   pais_origen VARCHAR(40) NOT NULL
 );
 
-INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen)
-VALUES (1, 'Johan', 'Castillo', 'M', '2018-01-01', 'Venezuela'),
-       (2, 'Keymar', 'Perez', 'F', '2010-02-01', 'Venezuela');
+INSERT INTO autores (autor_id, nombre, apellido, seudonimo, genero, fecha_nacimiento, pais_origen)
+VALUES (1, 'Johan', 'Castillo', 'Jc Johan', 'M', '2018-01-01', 'Venezuela'),
+       (2, 'Keymar', 'Perez', '' ,'F', '2010-02-01', 'Venezuela');
 
 SELECT  * FROM autores;
