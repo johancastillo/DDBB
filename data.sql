ffb6be946e7a0417bfbde1b20f5bcdfcf2824af8
Johan Castillo <jcjohan2707@gmail.com>
2020-09-02 10:31:32 -0400

    Campos obligatorios con NOT NULL

--- data.sql
+++ data.sql
@@ -3,12 +3,12 @@ CREATE DATABASE libreria_cf;
 USE libreria_cf;
 
 CREATE TABLE autores(
-  autor_id INT,
-  nombre VARCHAR(25),
-  apellido VARCHAR(25),
-  genero CHAR(1),
-  fecha_nacimiento DATE,
-  pais_origen VARCHAR(40)
+  autor_id INT NOT NULL,
+  nombre VARCHAR(25) NOT NULL,
+  apellido VARCHAR(25) NOT NULL,
+  genero CHAR(1) NOT NULL,
+  fecha_nacimiento DATE NOT NULL,
+  pais_origen VARCHAR(40) NOT NULL
 );
 
 INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen)
